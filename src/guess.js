4ed5182892c855fea352c477d4e9a3e16746d604
Libby Hamlin <bravolibby@gmail.com>
2019-07-18 09:11:08 -0700

    updated guess.js

--- src/guess.js
+++ src/guess.js
@@ -1,5 +1,12 @@
 const submitBtn = document.getElementById('submit-btn');
+const numberInput = document.getElementById('number-input');
+const messageInput = document.getElementById('message');
+const correctImage = document.getElementById('correct-image');
+const incorrectImage = document.getElementById('incorrect-image');
+const correctNumber = 3;
+
+
 submitBtn.addEventListener('click', () => {
-//console.log('hello');
-let correctNumber = 
+//console.log('works');
+
 });
